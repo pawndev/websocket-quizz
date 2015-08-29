fcb36c898d9284f6c4b0fcc2ee08d38da7395119
coquel_d <christophe1.coquelet@epitech.eu>
2015-08-29 11:03:29 +0200

    Ajout de la base de donnee

---
+++ quizz-ws.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Sam 29 Août 2015 à 11:02
+-- Version du serveur :  5.6.25-0ubuntu0.15.04.1
+-- Version de PHP :  5.6.4-4ubuntu6.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `quizz-ws`
+--
+CREATE DATABASE IF NOT EXISTS `quizz-ws` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `quizz-ws`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `question`
+--
+
+CREATE TABLE IF NOT EXISTS `question` (
+`id_quedtion` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `res1` varchar(255) NOT NULL,
+  `res2` varchar(255) NOT NULL,
+  `res3` varchar(255) NOT NULL,
+  `res4` varchar(255) NOT NULL,
+  `goodRes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `session`
+--
+
+CREATE TABLE IF NOT EXISTS `session` (
+  `id_session` int(11) NOT NULL,
+  `user` varchar(100) NOT NULL,
+  `id_question` int(11) NOT NULL,
+  `resSent` int(11) NOT NULL,
+  `time` varchar(10) NOT NULL,
+  `score` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `question`
+--
+ALTER TABLE `question`
+ ADD PRIMARY KEY (`id_quedtion`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `question`
+--
+ALTER TABLE `question`
+MODIFY `id_quedtion` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
