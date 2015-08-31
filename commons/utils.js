a58794a3aa353c2c813e126a95296a70c380706b
Mathias Desloges <freakdev@gmail.com>
2015-08-31 11:41:44 +0200

    add utils lib (node/browser compatible)

---
+++ commons/utils.js
@@ -0,0 +1,15 @@
+(function () {
+	
+	var utils = {
+		toArray: function (obj) {
+			return Array.prototype.slice.apply(obj);
+		}
+	}
+
+	if (typeof module === "object" && module && typeof module.exports === "object") {
+		module.exports = utils;
+	} else {
+		define(utils);
+	}
+
+})();
