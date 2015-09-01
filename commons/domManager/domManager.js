d208850f29162982cc33c7fe3d3b317d5392a101
Mathias Desloges <freakdev@gmail.com>
2015-09-02 00:30:57 +0200

    implement class management in domManager

--- commons/domManager/domManager.js
+++ commons/domManager/domManager.js
@@ -4,6 +4,8 @@ define(['../utils'], function (utils) {
         this._node = node;
         this._instructions = [];
         this._alive = true;
+
+        this._cssClassCache = node.className.split(' ');
     }
 
     // reference to "native" dom object
@@ -11,6 +13,8 @@ define(['../utils'], function (utils) {
 
     // virtual node should be destroyed ?
     DomNode.prototype.alive = false;
+
+    DomNode.prototype._cssClassCache = [];
 
     // for of instructions for a node
     DomNode.prototype._instructions = [];
@@ -35,12 +39,20 @@ define(['../utils'], function (utils) {
     }
 
     DomNode.prototype.refreshDom = function () {
+
+        var newClassName = this._cssClassCache.join(' ').trim();
+        if (newClassName !== this._node.className) {
+            this.prop('className', newClassName);
+        }
+
         this._instructions.forEach(function (instruction, id) {
             switch(instruction.type) {
                 case 'prop':
                     this._node[instruction.actions.key] = instruction.actions.value;
+                    break;
                 case 'attr':
                     this._node.setAttribute(instruction.actions.key, instruction.actions.value);
+                    break;
             }
         }, this);
 
@@ -51,20 +63,57 @@ define(['../utils'], function (utils) {
         this.alive = false;
     };
 
-    DomNode.prototype.getValue = function (attr, key) {
-        return this;
+    DomNode.prototype.getValue = function (key, isAttr) {
+        var instr, found = false, i = 0, len = this._instructions.length;
+        while (i < len && !found) {
+            instr = this._instructions[i];
+            if (instr.actions.key === key && (!isAttr && instr.type === 'prop' || isAttr && instr.type === 'attr')) {
+                return instr.actions.value;
+            }
+        }
+        return undefined;
     };   
 
     DomNode.prototype.attr = function (attr, value) {
+        if (arguments.length === 1)
+            return this.getValue(attr, true);
+
         this._addInstructions('attr', {key: attr, value: value});
+        return this;
     };
 
     DomNode.prototype.prop = function (prop, value) {
+        if (arguments.length === 1)
+            return this.getValue(prop);
+
         this._addInstructions('prop', {key: prop, value: value});
+        return this;
+    };
+
+    DomNode.prototype.addClass = function (value) {
+        if (!this.hasClass(value))
+            this._cssClassCache.push(value);
+    };
+
+    DomNode.prototype.removeClass = function (value) {
+        var indexOf = this._cssClassCache.indexOf(value);
+        if (indexOf !== -1)
+            this._cssClassCache.splice(indexOf, 1);
+    };
+
+    DomNode.prototype.toggleClass = function (value) {
+        if (!this.hasClass(value))
+            this.addClass(value);
+        else 
+            this.removeClass(value);
+    };
+
+    DomNode.prototype.hasClass = function (value) {
+        return this._cssClassCache.indexOf(value) !== -1 ? true : false;
     };
     
     var domManagerNodes = [],
-        KNOWN_INSTRUCTIONS = ['prop', 'attr']
+        KNOWN_INSTRUCTIONS = ['prop', 'attr', 'class']
         ;
         
 
