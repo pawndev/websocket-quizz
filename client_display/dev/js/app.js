c217b0545aa11f426f095c40cbc9d8dadb931e78
Mathias Desloges <freakdev@gmail.com>
2015-09-07 10:58:08 +0200

    implement client_display

---
+++ client_display/dev/js/app.js
@@ -0,0 +1,49 @@
+require(['domReady', 'socketio', '../../../commons/constants', '../../../commons/domManager/domManager', '../../../commons/pubsub/pubsub'], function (domReady, io, constants, dm, pubsub) {
+
+    var domBody,
+        listenerId;
+
+    domReady(function () {
+        dm.run();
+
+        domBody = dm.query('body');
+        domQuestion = dm.query('.question');
+        domAnswers = dm.queryAll('.answers');
+
+        listenerId = pubsub.subscribe(constants.MESSAGE.QUESTION_START, displayGameLayout);
+
+        /*domButtons = dm.queryAll('button');*/
+
+        domBody.addClass('wait');
+        console.log('init');
+    });
+
+    function displayGameLayout(param) {
+        pubsub.unsubscribe(constants.MESSAGE.QUESTION_START, listenerId);
+        domBody.removeClass('wait');
+        domBody.addClass('game-layout');
+        domQuestion.html(param.question);
+
+        domAnswers.forEach(function (domAnswer) {
+            domAnswer.html(param.answers[domAnswer.data('num') - 1]);
+        });
+
+        listenerId = pubsub.subscribe(constants.MESSAGE.TIMER_END, endTimer);
+    }
+
+    function endTimer() {
+        pubsub.unsubscribe(constants.MESSAGE.TIMER_END, listenerId);
+        domBody.removeClass('game-layout');
+        domBody.addClass('end-layout');
+        listenerId = pubsub.subscribe(constants.MESSAGE.RESULT_SENT, displayResult);
+    }
+
+    function displayResult (result) {
+        pubsub.unsubscribe(constants.MESSAGE.RESULT_SENT, listenerId);
+        domBody.removeClass('end-layout');
+        domBody.addClass('result-layout');
+    }
+
+    window.pubsub = pubsub;
+
+});
---
+++ client_display/dev/js/conf.js
@@ -0,0 +1,25 @@
+/*jslint browser:true */
+/*global require */
+
+var require = {
+    baseUrl: "../dev/js/",
+    paths: {
+        "domReady": '../../../bower_components/domReady/domReady',
+        "socketio": '../../../node_modules/socket.io/node_modules/socket.io-client/socket.io'
+/*        "underscore": '../../bower_components/underscore/underscore',
+        "deferred": '../../bower_components/underscore.deferred/underscore.deferred',
+        "text": '../../bower_components/requirejs-text/text',
+        "screenfull": '../../bower_components/screenfull/dist/screenfull'
+*/    },
+    shim: {
+/*        'underscore': {
+            exports: '_'
+        },
+        'deferred': {
+            exports: '_',
+        },
+        'screenfull': {
+            exports: 'screenfull'
+        }
+*/    }
+};
